bf7d9ec1f26525d8eee9772beb2cea894edc31dd
Russell Vea <russell@workwithgrid.com>
2015-10-08 16:10:16 -1000

    [#1] Adding bike paths.

--- public/app/src/components/Main.js
+++ public/app/src/components/Main.js
@@ -11,27 +11,58 @@ import {
 
 import {mapboxStyles} from '../globals/mapbox.js';
 import {connect} from 'fynx-decorators';
+// require('esri-leaflet/dist/esri-leaflet-src.js');
 
 let yeomanImage = require('../images/yeoman.png');
+
+const types = {
+  "Lane": {
+    color: "#FF851B",
+    width: 1,
+    opacity: 1
+  },
+  "Route": {
+    color: "#3D9970",
+    width: 1,
+    opacity: 1
+  },
+  "Path": {
+    color: "#FF851B",
+    width: 1,
+    opacity: 1
+  },
+  "Separated Bike Lane": {
+    color: "#FFDC00",
+    width: 3,
+    opacity: 1
+  }
+}
 
 @connect(store, 'data')
 class AppComponent extends React.Component {
   componentDidMount() {
-    actions.initRoutes();
     L.mapbox.accessToken = mapbox_access_token;
-    this.map = new L.mapbox.map('map', 'russellvea2.nl7ij7em', {
-      center: [21.456597,-157.857164],
-      zoom: 12
+    this.map = new L.mapbox.map('map', 'russellvea2.nlehndmb', {
+      center: [21.331, -157.777],
+      zoom: 13
     })
-  }
-
-  componentWillUpdate() {
+    let paths = L.esri.featureLayer({
+      url: 'http://services.arcgis.com/tNJpAOha4mODLkXz/ArcGIS/rest/services/BikePaths/FeatureServer/0',
+      where: '1=1',
+      style: (feature) => {
+        return types[feature.properties.Facility_T];
+      }
+    })
+    paths.addTo(this.map);
+    paths.bindPopup((feature)=> {
+      return feature.properties.Facility_N;
+    });
 
   }
 
   render() {
     return (
-      <div className="index" id="map" style={{height: '100vh'}}></div>
+      <div className="index" id="map" style={{height: '100vh', width: '100%'}}></div>
     );
   }
 }
